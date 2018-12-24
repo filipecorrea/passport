13c7f2c8cc2aac2f28177f54efd11d76359e5bb2
Filipe Corrêa <filipecorrea@br.ibm.com>
2018-12-24 16:17:14 -0200

    Add JSON database and encrypt users password

--- server/server.js
+++ server/server.js
@@ -5,42 +5,35 @@ const FileStore = require('session-file-store')(session)
 const bodyParser = require('body-parser')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const axios = require('axios')
+const bcrypt = require('bcrypt-nodejs')
 
-const users = [
-  {
-    id: '2f24vvg',
-    email: 'test@test.com',
-    password: 'password'
-  }
-]
-
-// configure passport.js to use the local strategy
 passport.use(new LocalStrategy(
   { usernameField: 'email' },
   (email, password, done) => {
-    console.log('Inside local strategy callback')
-    // here is where you make a call to the database
-    // to find the user based on their username or email address
-    // for now, we'll just pretend we found that it was users[0]
-    const user = users[0] 
-    if(email === user.email && password === user.password) {
-      console.log('Local strategy returned true')
-      return done(null, user)
-    }
+    axios.get(`http://localhost:5000/users?email=${email}`)
+      .then(res => {
+        const user = res.data[0]
+        if (!user) {
+          return done(null, false, { message: 'Invalid credentials.\n' })
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false, { message: 'Invalid credentials.\n' })
+        }
+        return done(null, user)
+      })
+      .catch(error => done(error))
   }
 ))
 
-// tell passport how to serialize the user
 passport.serializeUser((user, done) => {
-  console.log('Inside serializeUser callback. User id is save to the session file store here')
   done(null, user.id)
 })
 
 passport.deserializeUser((id, done) => {
-  console.log('Inside deserializeUser callback')
-  console.log(`The user id passport saved in the session file store is: ${id}`)
-  const user = users[0].id === id ? users[0] : false
-  done(null, user)
+  axios.get(`http://localhost:5000/users/${id}`)
+    .then(res => done(null, res.data))
+    .catch(error => done(error, false))
 })
 
 const app = express()
@@ -60,39 +53,29 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.get('/', (req, res) => {
-  console.log('Session ID:', req.sessionID)
-  res.send('You hit home page\n')
+  res.send(`You got home page!\n`)
 })
 
 app.get('/login', (req, res) => {
-  console.log('Inside GET /login callback')
-  console.log(req.sessionID)
   res.send(`You got the login page!\n`)
 })
 
 app.post('/login', (req, res, next) => {
-  console.log('Inside POST /login callback')
   passport.authenticate('local', (err, user, info) => {
-    console.log('Inside passport.authenticate() callback')
-    console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
-    console.log(`req.user: ${JSON.stringify(req.user)}`)
+    if (info) { return res.send(info.message) }
+    if (err) { return next(err) }
+    if (!user) { return res.redirect('/login') }
     req.login(user, (err) => {
-      console.log('Inside req.login() callback')
-      console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
-      console.log(`req.user: ${JSON.stringify(req.user)}`)
-      return res.send('You were authenticated & logged in!\n')
+      if (err) { return next(err) }
+      return res.redirect('/authrequired')
     })
-  })(req, res, next);
+  })(req, res, next)
 })
 
 app.get('/authrequired', (req, res) => {
-  console.log('Inside GET /authrequired callback')
-  console.log(`User authenticated? ${req.isAuthenticated()}`)
-  if(req.isAuthenticated()) {
-    res.send('you hit the authentication endpoint\n')
-  } else {
-    res.redirect('/')
-  }
+  (req.isAuthenticated())
+    ? res.send('you hit the authenticated endpoint\n')
+    : res.redirect('/')
 })
 
 app.listen(3000, () => console.log('Listening on localhost:3000'))
